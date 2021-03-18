21f0ba19e90a2b598a513fd4b00688eda06768d8
xibaochat <wmmxxbb@gmail.com>
2021-03-18 16:01:15 +0100

    simplifier strcmp

--- x64/devoir/ft_strcmp.s
+++ x64/devoir/ft_strcmp.s
@@ -3,24 +3,27 @@ section .text
 
 ft_strcmp:
     mov rax, 0
+	mov rcx, -1
 	mov r8, rdi
 	mov r9, rsi
-	mov rcx, -1
     jmp compare
 
 compare:
 	inc rcx
+
     cmp BYTE [r8 + rcx], 0
-	je exit
+    je exit
+
 	mov al, BYTE [r8 + rcx]
     cmp al, BYTE [r9 + rcx]
     je compare
 
 exit:
 	mov al, BYTE [r8 + rcx]
-	sub al, BYTE [r9 + rcx]
-	jnc ft_end
+    sub al, BYTE [r9 + rcx]
+	jc fini_exit
+    ret
+fini_exit:
 	neg al
-	neg eax
-ft_end:
+	neg rax
 	ret
